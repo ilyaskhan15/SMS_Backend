bff319ae71bf4ad08319840ccf41bfc7a21fdc29
ilyaskhan15 <ilyaskhanqwer0088@gmail.com>
2025-05-25 16:48:04 +0500

    make little bit change in the urls.py

--- SMS_CORE/urls.py
+++ SMS_CORE/urls.py
@@ -21,4 +21,5 @@ from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('parents/', include('school_app.urls'))
 ] + debug_toolbar_urls()
--- school_app/views.py
+++ school_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Parent
+
+def Guardian(request):
+    query_set = Parent.objects.all()
+    for parent in query_set:
+        print(parent)
+    return HttpResponse("Hello world")
 
 # Create your views here.
