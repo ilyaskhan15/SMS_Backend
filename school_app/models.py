44711fb33425405889accfaa1149d9195ef9b2a6
ilyaskhan15 <ilyaskhanqwer0088@gmail.com>
2025-05-25 15:18:30 +0500

    rename database table from school_app_parents to students guardian in models.py

--- school_app/models.py
+++ school_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+class Parent(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+    number = models.CharField(max_length=15)
+    email = models.EmailField()
+    class Meta:
+        db_table = 'students_guardians'
+
 # Create your models here.
