67e73785c8dd173b6f84c96abc16ad35ae4cf4b1
ilyaskhan15 <ilyaskhanqwer0088@gmail.com>
2025-05-25 18:42:04 +0500

    modifies parents models and views and index.html of it

--- school_app/models.py
+++ school_app/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
 class Parent(models.Model):
-    name = models.CharField(max_length=100, db_index=True)
+    first_name = models.CharField(max_length=100, null=False, blank=False, db_index=True)
+    last_name = models.CharField(max_length=100, null=False, blank=False, db_index=True)
     number = models.CharField(max_length=15)
     email = models.EmailField()
     class Meta:
         db_table = 'students_guardians'
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.number})"
 
 # Create your models here.
--- school_app/views.py
+++ school_app/views.py
@@ -1,10 +1,9 @@
+from django.core.exceptions import ObjectDoesNotExist 
 from django.shortcuts import render
 from .models import Parent
 
 
 def Guardian(request):
-    query_set = Parent.objects.all()
-    for parent in query_set:
-        print(parent)
-    return render(request, 'index.html', {'parents': "Ilyas"})
+   query_set = Parent.objects.filter(id__lt=10).order_by('first_name')
+   return render(request, 'index.html', {'name': "Ilyas", 'parents' : query_set})
 # Create your views here.
