d3fdf2ef7d1bb841034458910f55681d94ed2d22
ilyaskhan15 <ilyaskhanqwer0088@gmail.com>
2025-06-01 16:51:00 +0500

    add rest_framework in the  installapp

--- SMS_Project/settings.py
+++ SMS_Project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'SMS_App',  # Custom application for the SMS project
 ]
 
